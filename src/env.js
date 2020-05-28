1a9741fd14cd35de86f394d634982236d63dc7f2
dianamariand92 <dianamariand92@gmail.com>
2020-05-28 14:54:40 +0200

    first commit

---
+++ src/env.js
@@ -0,0 +1,9 @@
+module.exports = {
+    provider_url: 'https://aai-dev.egi.eu',
+    client_id: 'b98216d1-8b74-4090-a268-e7762e777ab6',
+    token: 'ALpudTDCKL6OZm5Wv_IV6Y-BuUkLyw_wKEzMbWFrny-TWvUo3cnUgrl9wt5UmKfkGrdKz894dzEmHyVF3KqoHss',
+    // redirect_uri:window.location.origin + '/callback.html'
+    redirect_uri: 'http://localhost:8080/callback.html',
+    url_authorize: 'https://aai-dev.egi.eu/oidc/authorize',
+    url_user_info: 'https://aai-dev.egi.eu/oidc/userinfo',
+}
